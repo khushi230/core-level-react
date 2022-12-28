ddec4f0ae3072fa0c241e2c939e2aba8a27acd49
khushi230 <khushboosirsa99@gmail.com>
2022-12-28 12:48:47 +0530

    added styling in props

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 const heading = React.createElement(
   "h1",
-  { id: "heading 1", key: "heading1" },
+  { id: "heading 1", key: "heading1", style: { color: "red" } },
   "Heading 1"
 );
 const headingTwo = React.createElement(
   "h2",
-  { id: "heading 2", key: "heading2" },
+  { id: "heading 2", key: "heading2", style: { color: "blue" } },
   "Heading 2"
 );
 const container = React.createElement("div", { id: "container" }, [
